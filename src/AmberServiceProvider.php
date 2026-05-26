0c92d2f65699dce6e99dc7a074ed00c7e6b1e287
Samuel Georges <sam@daftspunk.com>
2026-05-26 19:01:38 +1000

    scaffold service provider

---
+++ src/AmberServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace October\Amber;
+
+use Illuminate\Support\ServiceProvider;
+
+class AmberServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
